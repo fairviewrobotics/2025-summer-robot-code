3693c829d0e3a7945c1af3db3ad13b588ac1db3c
seoyool-l <seoyool009@gmail.com>
2025-09-25 19:50:39 -0600

    More shooter data

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,6 +4,8 @@ import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.networktables.DoubleEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.ShooterConstants;
 import frc.robot.utils.ConfigManager;
@@ -14,6 +16,7 @@ public class ShooterSubsystem extends SubsystemBase {
 
     private final SparkFlex topShooterMotor = new SparkFlex(ShooterConstants.topShooterMotorID, SparkLowLevel.MotorType.kBrushless);
     private final SparkFlex bottomShooterMotor = new SparkFlex(ShooterConstants.bottomShooterMotorID, SparkLowLevel.MotorType.kBrushless);
+    private final SparkFlex indexerMotor = new SparkFlex(ShooterConstants.indexerMotorID, SparkLowLevel.MotorType.kBrushless);
 
     private final SimpleMotorFeedforward shooterFF = new SimpleMotorFeedforward(
             config.get("ShooterFFKs", ShooterConstants.shooterFFKs),
@@ -24,6 +27,9 @@ public class ShooterSubsystem extends SubsystemBase {
     private final PIDController shooterPID = new PIDController(
             ShooterConstants.shooterP, ShooterConstants.shooterI, ShooterConstants.shooterD
     );
+
+    private final DoubleEntry shooterSetPoint = NetworkTableInstance.getDefault().getTable("Shooter").getDoubleTopic("shooter set point").getEntry(shooterPID.getSetpoint());
+    private final DoubleEntry shooterError = NetworkTableInstance.getDefault().getTable("Shooter").getDoubleTopic("shooter error").getEntry(shooterPID.getError());
 
     public ShooterSubsystem() {
 
@@ -39,6 +45,7 @@ public class ShooterSubsystem extends SubsystemBase {
                 shooterPID.calculate(MathUtils.RPMtoRadians(bottomShooterMotor.getEncoder().getVelocity()), MathUtils.RPMtoRadians(rpm)) +
                         shooterPID.calculate(MathUtils.RPMtoRadians(rpm))
         );
+        indexerMotor.set(rpm/2);
     }
 
     public void runVolts(double volts) {
